15e54b97bbd99d3caca54c3cda3a182928597b64
warriorcris25 <104987643+warriorcris25@users.noreply.github.com>
2023-08-08 23:52:29 -0500

    git add

---
+++ Sistemas/Sol_PuntoVenta/Sol_PuntoVenta.Datos/D_Punto_Venta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Sol_PuntoVenta.Entidades;
+
+namespace Sol_PuntoVenta.Datos
+{
+    public class D_Punto_Venta
+    {
+        public DataTable Listado_pv(string cTexto)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("procedimiento", SqlCon);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+    }
+}
